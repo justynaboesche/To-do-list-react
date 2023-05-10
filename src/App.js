764b5076aac48e94db62551293b67e05752be3f3
Justyna Boesche <justyna.boesche@gmail.com>
2023-05-10 11:23:15 +0200

    components and useTasks hook in App implemented

--- src/App.js
+++ src/App.js
@@ -1,9 +1,55 @@
+import Buttons from "./Buttons";
+import Form from "./Form";
+import Tasks from "./Tasks";
+import Section from "./Section";
+import Header from "./Header";
+import Container from "./Container";
+import { useTasks } from './useTasks';
+import { useState } from "react";
 
 function App() {
+  const [hideDone, setHideDone] = useState(false);
+
+  const toggleHideDone = () => {
+    setHideDone(hideDone => !hideDone);
+  };
+
+  const {
+    tasks,
+    removeTask,
+    toggleTaskDone,
+    setAllDone,
+    addNewTask,
+  } = useTasks();
+
   return (
-    <div>
-      Dzień dobry!
-    </div>
+    <Container>
+      <Header title="Lista zadań" />
+      <Section
+        title="Dodaj nowe zadanie"
+        body={<Form addNewTask={addNewTask} />}
+      />
+
+      <Section
+        title="Lista zadań"
+        body={
+          <Tasks
+            tasks={tasks}
+            hideDone={hideDone}
+            removeTask={removeTask}
+            toggleTaskDone={toggleTaskDone}
+          />
+        }
+        extraHeaderContent={
+          <Buttons
+            tasks={tasks}
+            hideDone={hideDone}
+            toggleHideDone={toggleHideDone}
+            setAllDone={setAllDone}
+          />
+        }
+      />
+    </Container>
   );
 }
 
